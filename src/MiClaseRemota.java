01be78d575a8b756a4f12bd8bcfec444a0c89335
FernandoGaribay <garibaycejaf@gmail.com>
2023-11-12 21:57:01 -0600

    + Cambios de IP

--- src/MiClaseRemota.java
+++ src/MiClaseRemota.java
@@ -1,3 +1,4 @@
+
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -6,23 +7,22 @@ import java.rmi.registry.Registry;
 public class MiClaseRemota extends UnicastRemoteObject implements MiInterfazRemota {
 
     public MiClaseRemota() throws RemoteException {
-    // Código del constructor
+        // Código del constructor
     }
 
     public void miMetodo1() throws RemoteException {
-    // Aquí ponemos el código que queramos
+        // Aquí ponemos el código que queramos
         System.out.println("Estoy en miMetodo1()");
     }
 
     public int miMetodo2() throws RemoteException {
-    // Aquí ponemos el código que queramos
-        System.out.println("Estoy en miMetodo1()");
+        // Aquí ponemos el código que queramos
         return 5;
     }
 
     public void otroMetodo() {
-    //
-    //
+        //
+        //
     }
 
     public static void main(String[] args) {
@@ -32,9 +32,17 @@ public class MiClaseRemota extends UnicastRemoteObject implements MiInterfazRemo
 
             MiInterfazRemota mir = new MiClaseRemota();
 
+            // Local Host
+            /*
             java.rmi.Naming.rebind("//"
                     + java.net.InetAddress.getLocalHost().getHostAddress()
-                    + ":" + "1234" + "/PruebaRMI", mir);
+                    + ":" + "1234" + "/PruebaRMI", mir); // args[0], args[1]
+            */
+
+            // Ip del servidor remoto
+            java.rmi.Naming.rebind("//"
+                    + "192.168.1.82"
+                    + ":" + "1234" + "/PruebaRMI", mir); // args[0] = , args[1]
         } catch (Exception e) {
             System.out.println(e);
         }
--- src/MiClienteRMI.java
+++ src/MiClienteRMI.java
@@ -4,9 +4,8 @@ public class MiClienteRMI {
 
     public static void main(String[] args) {
         try {
-            MiInterfazRemota mir
-                    = (MiInterfazRemota) Naming.lookup("//"
-                            + "127.0.0.1" + ":" + "1234" + "/PruebaRMI");
+            //MiInterfazRemota mir = (MiInterfazRemota)Naming.lookup("//" + args[0] + ":" + args[1] + "/PruebaRMI");
+            MiInterfazRemota mir = (MiInterfazRemota) Naming.lookup("//192.168.1.82:1234/PruebaRMI");
 
             // Imprimimos miMetodo1() tantas veces como devuelva miMetodo2()
             for (int i = 1; i <= mir.miMetodo2(); i++) {
